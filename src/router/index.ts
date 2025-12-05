7b6478c66db9fc264041f814fe32a2b37e28807e
Alex-20KD <gonzaloyzam.2018@gmail.com>
2025-12-05 12:10:16 -0500

    feat: todo bien

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/pets',
+      name: 'pets',
+      component: () => import('../views/PetsView.vue'),
+    },
   ],
 })
 
